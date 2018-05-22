6d12f019f4ca0bf4b13bad15c4d1eb5eacba8e04
Ryan Park <admin@ryanpark.co.uk>
2018-05-22 19:09:30 +0100

    Initial Commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'HealerHub',
+  description: 'Collaborative Healing Guides by the World of Warcraft Community',
+  themeConfig: {
+    lastUpdated: true,
+    sidebar: [
+      {
+        title: 'Discipline Priest',
+        children: [
+          '/Discipline-Priest/',
+          '/Discipline-Priest/Talents'
+        ]
+      },
+    ]
+  }
+}
